265e8fdfb920bac845852b47a8f70304148bcac1
TheoJansson <95386677+TheoJansson@users.noreply.github.com>
2021-12-02 00:09:08 +0100

    Update
    La till så det går att välja datum och sidan uppdateras.

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 //lZwg9tTZMqD86tp9mvKrQIFAjbrC4kpgVtKnCKp5
 //https://wilsjame.github.io/how-to-nasa/
 
-
 var req = new XMLHttpRequest();
 var url = "https://api.nasa.gov/planetary/apod?api_key=";
 var api_key = "lZwg9tTZMqD86tp9mvKrQIFAjbrC4kpgVtKnCKp5";
@@ -18,4 +17,26 @@ req.addEventListener("load", function(){
     document.getElementById("pic").src = response.hdurl;
     document.getElementById("explanation").textContent = response.explanation;
   }
-})
+
+});
+
+function handler(e){
+  var req = new XMLHttpRequest();
+  var url = "https://api.nasa.gov/planetary/apod?";
+  var api_key = "lZwg9tTZMqD86tp9mvKrQIFAjbrC4kpgVtKnCKp5";
+  var datum = e.target.value;
+
+  req.open("GET", url + "start_date=" + datum + "&end_date=" + datum + "&api_key=" + api_key, true);
+  req.send();
+
+  req.addEventListener("load", function(){
+    if(req.status == 200 && req.readyState == 4){
+      var response = JSON.parse(req.responseText);
+      document.getElementById("title").textContent = response[0].title;
+      document.getElementById("date").textContent = response[0].date;
+      document.getElementById("pic").src = response[0].hdurl;
+      document.getElementById("explanation").textContent = response[0].explanation;
+}
+});
+
+}
