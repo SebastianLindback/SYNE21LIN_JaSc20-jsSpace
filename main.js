4758badb694b8a28b5c6fa0641926bdd4e532eb6
Tobias Parnebrant <tobias.parnebrant.syne21lin@tucsweden.se>
2021-12-01 15:17:43 +0100

    Design

---
+++ main.js
@@ -0,0 +1,21 @@
+//https://api.nasa.gov/planetary/apod?api_key=lZwg9tTZMqD86tp9mvKrQIFAjbrC4kpgVtKnCKp5
+//lZwg9tTZMqD86tp9mvKrQIFAjbrC4kpgVtKnCKp5
+//https://wilsjame.github.io/how-to-nasa/
+
+
+var req = new XMLHttpRequest();
+var url = "https://api.nasa.gov/planetary/apod?api_key=";
+var api_key = "lZwg9tTZMqD86tp9mvKrQIFAjbrC4kpgVtKnCKp5";
+
+req.open("GET", url + api_key, true);
+req.send();
+
+req.addEventListener("load", function(){
+	if(req.status == 200 && req.readyState == 4){
+  	var response = JSON.parse(req.responseText);
+    document.getElementById("title").textContent = response.title;
+    document.getElementById("date").textContent = response.date;
+    document.getElementById("pic").src = response.hdurl;
+    document.getElementById("explanation").textContent = response.explanation;
+  }
+})
