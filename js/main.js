5b52e833d49da77974854ab23b6d941dcded9247
Sebastian <Sebastian@lindback.pro>
2021-12-02 17:00:41 +0100

    Doomsday prognos genom NeoWs api Listar passerande astroider inom de närmsta två dagarna och highlightar de med potenciell fara.

--- js/main.js
+++ js/main.js
@@ -6,38 +6,48 @@ var req = new XMLHttpRequest();
 var url = "https://api.nasa.gov/planetary/apod?api_key=";
 let neoStartDate = "";
 let neoEndDate = "";
-let neoWs = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${neoStartDate}&end_date=${neoEndDate}&api_key=`;
+todaysDate(); // definerar dagens datum i datumväljaren.
+neoWs = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${neoStartDate}&end_date=${neoEndDate}&api_key=`;
+
 var api_key = "lZwg9tTZMqD86tp9mvKrQIFAjbrC4kpgVtKnCKp5";
 let publicTemp;
-<<<<<<< HEAD
-
-todaysDate();
-sendAPIreq(url + api_key,1);
-
-=======
 let arrNamn = [];
+let doomsdayAstro = [];
 // Programmet körs imellan dessa funktioner
->>>>>>> 7f2857af8be7e1f89d8e97b5cada9c04c19dc953
 
 
+
+
+
+sendAPIreq(url + api_key,1); // kör APOS API
+
+
+//----
+// den här körs på onclick
 function handler(e){
+	// när datumet väljs i datumväljaren skickas läggs det till i filtreringen vid api call
   var hUrl = "https://api.nasa.gov/planetary/apod?";
   var datum = e.target.value;
 	sendAPIreq(hUrl + "start_date=" + datum + "&end_date=" + datum + "&api_key=" + api_key, 1);
 }
 
+
+
+
+
+//Nedan defineras funktionerna, men det körs enbart när de anropas, se åvan.
 async function sendAPIreq(fetchUrl, apiMetod) {
+	// funktionen tar emot data från api länk + api key och sedan skckar den ner datan som en JSON till apiDataUse
 	// apimetod: 1 = APOS, 0 = NeoWs
 	let req = await fetch(fetchUrl);
 	req.json().then(data => {
   console.log(data, "oki" + apiMetod);
 	apiDataUse(data, apiMetod);
 	});
-	console.log(req);
 
 }
 function apiDataUse(data, apiMetod){
-
+  doomsdayAstro = [];
 	let danger = 0;
 	if (apiMetod){
 		//APOS Metod
@@ -48,74 +58,51 @@ function apiDataUse(data, apiMetod){
 	document.getElementById("explanation").textContent = data.explanation;
 	}
 	else{
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> a226b6cc291a91deea41b0f3463fd9b855c1310d
->>>>>>> 7f2857af8be7e1f89d8e97b5cada9c04c19dc953
 		// NeoWs Metod
 		console.log(`Mellan ${neoStartDate} och ${neoEndDate} passerade ${data.element_count}st astroider.`);
 		// astroMin + astroMax består av två olika arrayer. en array för startdatumet (neoStartDate) och en för slutdatum (neoEndDate)
 		let astroMin = data.near_earth_objects[neoStartDate];
-    console.log(neoStartDate);
 		// skriver ut alla namn i arrayen med startdatumet och kollor ifall det fanns någon astroid med potenciell fara.
-		for (let i = 0; i < astroMin.length; i++){
+    for (let i = 0; i < astroMin.length; i++){
 			 console.log(astroMin[i].name);
-<<<<<<< HEAD
-       document.getElementById("neotext").textContent += astroMin[i].name;
-=======
 			 arrNamn.push(astroMin[i].name);
->>>>>>> 7f2857af8be7e1f89d8e97b5cada9c04c19dc953
-		  	if (astroMin.is_potentially_hazardous_asteroid){ danger++;}
-			}
+		 if (astroMin[i].is_potentially_hazardous_asteroid){ danger++;doomsdayAstro.push(astroMin[i]);} }
+
 
 		let astroMax = data.near_earth_objects[neoEndDate];
 		// skriver ut alla namn i arrayen med slutdatumet och kollor ifall det fanns någon astroid med potenciell fara.
 		for (let i = 0; i < astroMax.length; i++){
 			 console.log(astroMax[i].name);
-<<<<<<< HEAD
-document.getElementById("neotext").textContent += astroMax[i].name;
-=======
-<<<<<<< HEAD
 			 arrNamn.push(astroMax[i].name);
-=======
+		 if (astroMax[i].is_potentially_hazardous_asteroid){ danger++;doomsdayAstro.push(astroMax[i]);}
+   }
 
->>>>>>> a226b6cc291a91deea41b0f3463fd9b855c1310d
->>>>>>> 7f2857af8be7e1f89d8e97b5cada9c04c19dc953
-		 if (astroMax.is_potentially_hazardous_asteroid){ danger++;} }
-		}
 		// skriver ut resultatet av hur många som var farliga.
-		console.log(`${danger} av nämnda astroider var potenciellt farliga`);
-<<<<<<< HEAD
-    document.getElementById("closecol").textContent =`${danger} av nämnda astroider var potenciellt farliga`;
-=======
-		console.log(arrNamn);
 
+    let classDanger= "";
+    let link = "";
+    for (var i = 0; i < arrNamn.length; i++) {
+      for (var y = 0; y < doomsdayAstro.length; y++) {
+        if (arrNamn[i] == doomsdayAstro[y].name) { classDanger= `class='danger'`; console.log("danger");}
 
->>>>>>> 7f2857af8be7e1f89d8e97b5cada9c04c19dc953
+      }
+      link = `https://www.google.com/search?q=${arrNamn[i]}`;
+      document.getElementById("astroider").innerHTML += `<li ${classDanger}><a href='${link}'> ${arrNamn[i]}  <a/></li>`;
+      classDanger= "";
+      link = "";
+    }
+
+    document.getElementById("dangerText").innerHTML = (`${danger} av listade astroider är potenciellt farliga`);
+  }
 	}
 
 function todaysDate(){
 	// funktionen definerar dagens datum som ett max värde för datumväljaren med id=dt. så att programmet inte error uppstår av att framtida datum väljs.
-	var today = new Date();
-	let todayDate = today.getDate();
+  var today = new Date();
+    neoStartDate = new Date(today.getFullYear(), today.getMonth(), today.getDate()+2).toISOString().split('T')[0];
+    neoEndDate = new Date(today.getFullYear(), today.getMonth(), (today.getDate()+3)).toISOString().split('T')[0];
+    document.getElementById("dt").max = new Date(today.getFullYear(), today.getMonth(), today.getDate()+1).toISOString().split('T')[0];
+    document.getElementById("dt").value = document.getElementById("dt").max;
+	console.log(neoStartDate, neoEndDate);
 
-	// datumet kommer i fel format, xxxx-xx-x vi behöver xxxx-xx-xx.
-	if (String(todayDate).length == 1) {todayDate = `0${todayDate}`;}
-	console.log(todayDate.length, todayDate);
-	var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+todayDate;
-	document.getElementById("dt").max = date;
-  if (todayDate > 2) {
-  neoStartDate =today.getFullYear()+'-'+(today.getMonth()+1)+'-'+todayDate - 2;
-  }
-  else{
-    let månad = today.getMonth()+1;
-    neoStartDate =today.getFullYear()+'-'+(månad- 1)+'-'+(todayDate - 2 + 30);
-    console.log(neoStartDate);
-  }
-  neoEndDate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+(todayDate - 1);
 }
