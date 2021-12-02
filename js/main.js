0fd48fe6dbac4078ba6dc72ee9edb5536e7b9ee1
Sebastian <Sebastian@lindback.pro>
2021-12-02 11:59:27 +0100

    Clean script

--- js/main.js
+++ js/main.js
@@ -4,39 +4,45 @@
 
 var req = new XMLHttpRequest();
 var url = "https://api.nasa.gov/planetary/apod?api_key=";
+let neoStartDate = "2019-10-11";
+let neoEndDate = "2019-10-12";
+let neoWs = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${neoStartDate}&end_date=${neoEndDate}&api_key=`;
 var api_key = "lZwg9tTZMqD86tp9mvKrQIFAjbrC4kpgVtKnCKp5";
 
-req.open("GET", url + api_key, true);
-req.send();
 
-req.addEventListener("load", function(){
-	if(req.status == 200 && req.readyState == 4){
-  	var response = JSON.parse(req.responseText);
-    document.getElementById("title").textContent = response.title;
-    document.getElementById("date").textContent = response.date;
-    document.getElementById("pic").src = response.hdurl;
-    document.getElementById("explanation").textContent = response.explanation;
-  }
-
-});
+todaysDate();
+sendAPIreq(url + api_key);
 
 function handler(e){
-  var req = new XMLHttpRequest();
-  var url = "https://api.nasa.gov/planetary/apod?";
-  var api_key = "lZwg9tTZMqD86tp9mvKrQIFAjbrC4kpgVtKnCKp5";
+  var hUrl = "https://api.nasa.gov/planetary/apod?";
   var datum = e.target.value;
-
-  req.open("GET", url + "start_date=" + datum + "&end_date=" + datum + "&api_key=" + api_key, true);
-  req.send();
-
-  req.addEventListener("load", function(){
-    if(req.status == 200 && req.readyState == 4){
-      var response = JSON.parse(req.responseText);
-      document.getElementById("title").textContent = response[0].title;
-      document.getElementById("date").textContent = response[0].date;
-      document.getElementById("pic").src = response[0].hdurl;
-      document.getElementById("explanation").textContent = response[0].explanation;
+	sendAPIreq(hUrl + "start_date=" + datum + "&end_date=" + datum + "&api_key=" + api_key, 1);
 }
-});
 
+async function sendAPIreq(fetchUrl, click) {
+	let req = await fetch(fetchUrl);
+	req.json().then(data => {
+  console.log(data, "oki" + click);
+	console.log(data.date);
+	apiDataUse(data, click);
+	});
+	console.log(req);
+
+}
+function apiDataUse(data, click){
+	if (click){
+	if (data.date == undefined) {data = data[0]}
+	document.getElementById("title").textContent = data.title;
+	document.getElementById("date").textContent = data.date;
+	document.getElementById("pic").src = data.hdurl;
+	document.getElementById("explanation").textContent = data.explanation;
+	}
+}
+function todaysDate(){
+	var today = new Date();
+	let todayDate = today.getDate();
+	if (String(todayDate).length == 1) {todayDate = `0${todayDate}`;}
+	console.log(todayDate.length, todayDate);
+	var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+todayDate;
+	document.getElementById("dt").max = date;
 }
