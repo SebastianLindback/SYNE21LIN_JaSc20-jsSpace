b490423cca6e8dbb813037a70541737cc8112e7b
TheoJansson <95386677+TheoJansson@users.noreply.github.com>
2021-12-02 19:47:18 +0100

    Quality of Life
    + Gjorde BG lite mindre så sidan fungerar som en one page (innan kunde man scrolla ner lite)
    + Scrollar ner automatiskt efter man klickat på "Ladda in prognos"

--- js/main.js
+++ js/main.js
@@ -4,39 +4,112 @@
 
 var req = new XMLHttpRequest();
 var url = "https://api.nasa.gov/planetary/apod?api_key=";
+let neoStartDate = "";
+let neoEndDate = "";
+todaysDate(); // definerar dagens datum i datumväljaren.
+neoWs = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${neoStartDate}&end_date=${neoEndDate}&api_key=`;
+
 var api_key = "lZwg9tTZMqD86tp9mvKrQIFAjbrC4kpgVtKnCKp5";
+let publicTemp;
+let arrNamn = [];
+let doomsdayAstro = [];
+// Programmet körs imellan dessa funktioner
 
-req.open("GET", url + api_key, true);
-req.send();
 
-req.addEventListener("load", function(){
-	if(req.status == 200 && req.readyState == 4){
-  	var response = JSON.parse(req.responseText);
-    document.getElementById("title").textContent = response.title;
-    document.getElementById("date").textContent = response.date;
-    document.getElementById("pic").src = response.hdurl;
-    document.getElementById("explanation").textContent = response.explanation;
-  }
 
-});
 
+
+sendAPIreq(url + api_key,1); // kör APOS API
+
+
+//----
+// den här körs på onclick
 function handler(e){
-  var req = new XMLHttpRequest();
-  var url = "https://api.nasa.gov/planetary/apod?";
-  var api_key = "lZwg9tTZMqD86tp9mvKrQIFAjbrC4kpgVtKnCKp5";
+	// när datumet väljs i datumväljaren skickas läggs det till i filtreringen vid api call
+  var hUrl = "https://api.nasa.gov/planetary/apod?";
   var datum = e.target.value;
-
-  req.open("GET", url + "start_date=" + datum + "&end_date=" + datum + "&api_key=" + api_key, true);
-  req.send();
-
-  req.addEventListener("load", function(){
-    if(req.status == 200 && req.readyState == 4){
-      var response = JSON.parse(req.responseText);
-      document.getElementById("title").textContent = response[0].title;
-      document.getElementById("date").textContent = response[0].date;
-      document.getElementById("pic").src = response[0].hdurl;
-      document.getElementById("explanation").textContent = response[0].explanation;
+	sendAPIreq(hUrl + "start_date=" + datum + "&end_date=" + datum + "&api_key=" + api_key, 1);
 }
-});
+
+function clearBox(elementID)
+{
+	arrNamn.length = 0; // nollställ 
+    document.getElementById(elementID).innerHTML = ""; // ta bort alla Asteroider
+}
+
+//Nedan defineras funktionerna, men det körs enbart när de anropas, se åvan.
+async function sendAPIreq(fetchUrl, apiMetod) {
+	// funktionen tar emot data från api länk + api key och sedan skckar den ner datan som en JSON till apiDataUse
+	// apimetod: 1 = APOS, 0 = NeoWs
+	let req = await fetch(fetchUrl);
+	req.json().then(data => {
+  console.log(data, "oki" + apiMetod);
+	apiDataUse(data, apiMetod);
+	});
+
+}
+
+function apiDataUse(data, apiMetod){
+clearBox("astroider");
+  doomsdayAstro = [];
+	let danger = 0;
+	if (apiMetod){
+		//APOS Metod
+	if (data.date == undefined) {data = data[0]}
+	document.getElementById("title").textContent = data.title;
+	document.getElementById("date").textContent = data.date;
+	document.getElementById("pic").src = data.hdurl;
+	document.getElementById("explanation").textContent = data.explanation;
+	}
+	else{
+		// NeoWs Metod
+		console.log(`Mellan ${neoStartDate} och ${neoEndDate} passerade ${data.element_count}st astroider.`);
+		// astroMin + astroMax består av två olika arrayer. en array för startdatumet (neoStartDate) och en för slutdatum (neoEndDate)
+		let astroMin = data.near_earth_objects[neoStartDate];
+		// skriver ut alla namn i arrayen med startdatumet och kollor ifall det fanns någon astroid med potenciell fara.
+    for (let i = 0; i < astroMin.length; i++){
+			 console.log(astroMin[i].name);
+			 arrNamn.push(astroMin[i].name);
+		 if (astroMin[i].is_potentially_hazardous_asteroid){ danger++;doomsdayAstro.push(astroMin[i]);} }
+
+
+		let astroMax = data.near_earth_objects[neoEndDate];
+		// skriver ut alla namn i arrayen med slutdatumet och kollor ifall det fanns någon astroid med potenciell fara.
+		for (let i = 0; i < astroMax.length; i++){
+			 console.log(astroMax[i].name);
+			 arrNamn.push(astroMax[i].name);
+		 if (astroMax[i].is_potentially_hazardous_asteroid){ danger++;doomsdayAstro.push(astroMax[i]);}
+   }
+
+  // skriver ut resultatet av hur många som var farliga.
+    let classDanger= "";
+    let link = "";
+    for (var i = 0; i < arrNamn.length; i++) {
+      for (var y = 0; y < doomsdayAstro.length; y++) {
+        if (arrNamn[i] == doomsdayAstro[y].name) { classDanger= `class='danger'`; console.log("danger");}
+        link = `https://www.google.com/search?q=${arrNamn[i]}`;
+      }
+      link = `https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley`;
+      document.getElementById("astroider").innerHTML += `<li ${classDanger}><a href='${link}'> ${arrNamn[i]}  <a/></li>`;
+      classDanger= "";
+      link = "";
+    }
+
+    document.getElementById("dangerText").innerHTML = (`${danger} av listade astroider är potenciellt farliga`);
+
+	// scrolla ner automatiskt till astroiderna
+	window.scrollTo(0,document.body.scrollHeight);
+
+  }
+	}
+
+function todaysDate(){
+	// funktionen definerar dagens datum som ett max värde för datumväljaren med id=dt. så att programmet inte error uppstår av att framtida datum väljs.
+  var today = new Date();
+    neoStartDate = new Date(today.getFullYear(), today.getMonth(), today.getDate()+2).toISOString().split('T')[0];
+    neoEndDate = new Date(today.getFullYear(), today.getMonth(), (today.getDate()+3)).toISOString().split('T')[0];
+    document.getElementById("dt").max = new Date(today.getFullYear(), today.getMonth(), today.getDate()+1).toISOString().split('T')[0];
+    document.getElementById("dt").value = document.getElementById("dt").max;
+	console.log(neoStartDate, neoEndDate);
 
 }
